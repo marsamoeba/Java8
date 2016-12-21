4de94deef9cb6087ce2ce405ba385c5a0dada6f1
marsamoeba <305609342@qq.com>
2016-12-21 11:59:39 +0800

    System.out.println("a = " + a);

---
+++ src/chapter04/OpEquals.java
@@ -0,0 +1,24 @@
+package chapter04;
+
+/**
+ * p64 赋值运算符
+ * @author marsamoeba
+ *
+ */
+// Demonstrate serveral assignment operators.
+public class OpEquals {
+	public static void main(String[] args) {
+		int a = 1;
+		int b = 2;
+		int c = 3;
+		
+		a += 5;
+		b *= 4;
+		c += a * b;
+		c %= 6;
+		
+		System.out.println("a = " + a);
+		System.out.println("b = " + b);
+		System.out.println("c = " + c);
+	}
+}
