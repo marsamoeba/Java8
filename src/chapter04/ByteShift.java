5610abaf22146099c51c1c976c1fe6abc0d9348d
marsamoeba <305609342@qq.com>
2016-12-21 14:51:01 +0800

    p70 左移 Byte

---
+++ src/chapter04/ByteShift.java
@@ -0,0 +1,19 @@
+package chapter04;
+
+/**
+ * p70 左移 Byte
+ * @author marsamoeba
+ *
+ */
+// Left shifting a byte value.
+public class ByteShift {
+	public static void main(String[] args) {
+		byte a = 64, b;
+		int i;
+		
+		i = a << 2;
+		b = (byte) (a << 2);
+		System.out.println("Original value of a: " + a);
+		System.out.println("i and b: " + i + " " + b);
+	}
+}
