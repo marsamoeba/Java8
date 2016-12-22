0a0fc700473cd1e57b38bcc36b347c0f55163511
marsamoeba <305609342@qq.com>
2016-12-22 18:31:41 +0800

    p84 if-else-if语句

---
+++ src/chapter05/IfElse.java
@@ -0,0 +1,26 @@
+package chapter05;
+
+/**
+ * p84 if-else-if语句
+ * @author marsamoeba
+ *
+ */
+// Demonstrate if-else-if statements.
+public class IfElse {
+	public static void main(String[] args) {
+		int month = 4; // April
+		String season;
+		
+		if (month == 12 || month == 1 || month == 2)
+			season = "Winter";
+		else if (month == 3 || month == 4 || month == 5)
+			season = "Spring";
+		else if (month == 6 || month == 7 || month == 8)
+			season = "Summer";
+		else if (month == 9 || month == 10 || month == 11)
+			season = "Autumn";
+		else 
+			season = "Bogus Month";
+		System.out.println("April is in the " + season + ".");
+	}
+}
