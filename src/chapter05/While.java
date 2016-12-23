55189ee3fb98747425ef282eac97af8d71210933
marsamoeba <305609342@qq.com>
2016-12-23 09:38:35 +0800

    p90 while语句

---
+++ src/chapter05/While.java
@@ -0,0 +1,18 @@
+package chapter05;
+
+/**
+ * p90 while语句
+ * @author marsamoeba
+ *
+ */
+// Demonstrate the while loop.
+public class While {
+	public static void main(String[] args) {
+		int n = 10;
+		
+		while (n > 0) {
+			System.out.println("tick " + n);
+			n--;
+		}
+	}
+}
