64559ec6b98b1d6624dfe0ed5c40bee53b95f9ea
marsamoeba <305609342@qq.com>
2016-12-24 15:19:33 +0800

    p94 for

---
+++ src/chapter05/ForTick.java
@@ -0,0 +1,13 @@
+package chapter05;
+
+/**
+ * p94 for
+ * @author marsamoeba
+ *
+ */
+public class ForTick {
+	public static void main(String[] args) {
+		for (int n = 10; n > 0; n--) 
+			System.out.println("tick " + n);
+	}
+}
