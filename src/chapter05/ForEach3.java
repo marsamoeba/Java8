e83766ea3a48881b228b7bba52e19c84211d67a5
marsamoeba <305609342@qq.com>
2016-12-25 22:28:56 +0800

    p100 for-each风格for循环对多维数组进行迭代

---
+++ src/chapter05/ForEach3.java
@@ -0,0 +1,27 @@
+package chapter05;
+
+/**
+ * p100 for-each风格for循环对多维数组进行迭代
+ * @author marsamoeba
+ *
+ */
+// Use for-each style for on a two-dimensional array.
+public class ForEach3 {
+	public static void main(String[] args) {
+		int sum = 0;
+		int nums[][] = new int[3][5];
+		
+		for (int i = 0; i < 3; i++) 
+			for (int j = 0; j < 5; j++)
+				nums[i][j] = (i + 1) * (j + 1);
+		
+		for (int x[] : nums) {
+			for (int y : x) {
+				System.out.println("Value is: " + y);
+				sum += y;
+			}
+		}
+		
+		System.out.println("Summation: " + sum);
+	}
+}
