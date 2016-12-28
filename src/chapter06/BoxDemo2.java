949bfc8d163bee52d2946dfcf4d295e8f31d185b
marsamoeba <305609342@qq.com>
2016-12-28 21:01:13 +0800

    p114

--- src/chapter06/BoxDemo2.java
+++ src/chapter06/BoxDemo2.java
@@ -5,7 +5,7 @@ package chapter06;
  * @author marsamoeba
  *
  */
-public class BoxDemo2 {
+class BoxDemo2 {
 	public static void main(String[] args) {
 		Box mybox1 = new Box();
 		Box mybox2 = new Box();
