6a554672c15e5270ddc11d1bae356772d4e4ad7c
marsamoeba <305609342@qq.com>
2016-12-28 20:57:37 +0800

    p113 简单的类

--- src/chapter06/BoxDemo.java
+++ src/chapter06/BoxDemo.java
@@ -6,10 +6,16 @@ package chapter06;
  * @author marsamoeba
  *
  */
+// This program includes a method inside the box class.
 class Box {
 	double width;
 	double height;
 	double depth;
+	
+	void volume () {
+		System.out.println("Volume is ");
+		System.out.println(width * height * depth);
+	}
 }
 
 class BoxDemo {
