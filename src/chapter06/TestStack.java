32295d7270233120b139fc255ea0aed23be84b0a
marsamoeba <305609342@qq.com>
2016-12-29 23:38:58 +0800

    p130 堆栈测试类

---
+++ src/chapter06/TestStack.java
@@ -0,0 +1,13 @@
+package chapter06;
+
+/**
+ * p130 堆栈测试类
+ * @author marsamoeba
+ *
+ */
+public class TestStack {
+	public static void main(String[] args) {
+		Stack mystack1 = new Stack();
+		Stack mystack2 = new Stack();
+	}
+}
