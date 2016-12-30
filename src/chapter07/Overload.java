59d6adc8af95c749f4325a16790bf10d02b946c3
marsamoeba <305609342@qq.com>
2016-12-30 17:17:40 +0800

    p132 方法的重载

--- src/chapter07/Overload.java
+++ src/chapter07/Overload.java
@@ -7,14 +7,25 @@ package chapter07;
  */
 public class Overload {
 	public static void main(String[] args) {
+//		OverloadDemo ob = new OverloadDemo();
+//		double result;
+		
+//		ob.test();
+//		ob.test(10);
+//		ob.test(10, 20);
+//		result = ob.test(123.25);
+//		
+//		System.out.println("Result of ob.test(123.25): " + result);
+		
+		
 		OverloadDemo ob = new OverloadDemo();
-		double result;
+		int i = 88;
 		
 		ob.test();
-		ob.test(10);
 		ob.test(10, 20);
-		result = ob.test(123.25);
 		
-		System.out.println("Result of ob.test(123.25): " + result);
+		ob.test(i);
+		ob.test(123.2);
+		
 	}
 }
