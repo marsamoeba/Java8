2554539acecef3a256d38189f26680ac783a0fc4
marsamoeba <305609342@qq.com>
2016-12-31 09:12:43 +0800

    Box类

--- src/chapter07/Box.java
+++ src/chapter07/Box.java
@@ -10,6 +10,13 @@ public class Box {
 	double height;
 	double depth;
 	
+	// OverloadCons2.java
+	Box(Box ob) {
+		width = ob.width;
+		height = ob.height;
+		depth = ob.depth;
+	}
+	
 	Box(double w, double h, double d) {
 		width = w;
 	    height = h;
