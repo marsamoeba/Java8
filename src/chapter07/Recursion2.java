aefdd95f8e6c55a4ef212b6939c991fdd0b30326
marsamoeba <305609342@qq.com>
2017-01-01 23:33:38 +0800

    p143 递归  详细步骤

--- src/chapter07/Recursion2.java
+++ src/chapter07/Recursion2.java
@@ -7,6 +7,19 @@ package chapter07;
  */
 // Another example that uses recursion.
 class RecTest {
+	int values[];
+	
+	RecTest(int i) {
+		values = new int[i];
+	}
+	
+	void printArray(int i) {
+		if (i == 0)
+			return;
+		else
+			printArray(i - 1);
+		System.out.println("[" + (i-1) + "] " + values[i-1]);
+	}
 	
 }
 class Recursion2 {
