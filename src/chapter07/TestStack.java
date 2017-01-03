f22947d3cf35dad8fdc23765f9c7304d83e6220d
marsamoeba <305609342@qq.com>
2017-01-04 00:46:24 +0800

    p146 Stack改进版测试类

--- src/chapter07/TestStack.java
+++ src/chapter07/TestStack.java
@@ -10,7 +10,22 @@ public class TestStack {
 		Stack mystack1 = new Stack();
 		Stack mystack2 = new Stack();
 		
+		for (int i = 0; i < 10; i++) 
+			mystack1.push(i);
+		for (int i = 0; i < 10; i++) 
+			mystack2.push(i);
 		
+		System.out.println("Stack in mystack1:");
+		for (int i = 0; i < 10; i++) 
+			System.out.println(mystack1.pop());
+		
+		System.out.println("Stack in mystack2:");
+		for (int i = 0; i < 10; i++) 
+			System.out.println(mystack2.pop());
+		
+		
+		//mystack1.tos = -2;
+		//mystack2.stck[3] = 100;
 	}
 
 }
