0e78766a6f70bd4ac0d9d8f825d3426028662bd3
marsamoeba <305609342@qq.com>
2017-01-03 23:54:39 +0800

    p146 Stack改进版测试类

---
+++ src/chapter07/TestStack.java
@@ -0,0 +1,16 @@
+package chapter07;
+
+/**
+ * p146 Stack改进版测试类
+ * @author marsamoeba
+ *
+ */
+public class TestStack {
+	public static void main(String[] args) {
+		Stack mystack1 = new Stack();
+		Stack mystack2 = new Stack();
+		
+		
+	}
+
+}
