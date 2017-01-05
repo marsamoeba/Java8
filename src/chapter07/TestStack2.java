73ec650ea64f80e45421bd4255004947af3db3b2
marsamoeba <305609342@qq.com>
2017-01-06 07:46:26 +0800

    p150 重新审视数组 堆栈改进版

---
+++ src/chapter07/TestStack2.java
@@ -0,0 +1,51 @@
+package chapter07;
+
+/**
+ * p150 重新审视数组 堆栈改进版
+ * @author marsamoeba
+ *
+ */
+// Improved Stack class that uses the length array member.
+class Stack2 {
+	  private int stck[];
+	  private int tos;
+	
+	  Stack2(int size) {
+	    stck = new int[size];
+	    tos = -1;
+	  }
+	
+	  void push(int item) {
+	    if(tos==stck.length-1) 
+	      System.out.println("Stack is full.");
+	    else 
+	      stck[++tos] = item;
+	  }
+	
+	  int pop() {
+	    if(tos < 0) {
+	      System.out.println("Stack underflow.");
+	      return 0;
+	    }
+	    else 
+	      return stck[tos--];
+	  }
+	}
+	
+class TestStack2 {
+	public static void main(String args[]) {
+	    Stack2 mystack1 = new Stack2(5);
+	    Stack2 mystack2 = new Stack2(8);
+	
+	    for(int i=0; i<5; i++) mystack1.push(i);
+	    for(int i=0; i<8; i++) mystack2.push(i);
+	
+	    System.out.println("Stack in mystack1:");
+	    for(int i=0; i<5; i++) 
+	       System.out.println(mystack1.pop());
+	
+	    System.out.println("Stack in mystack2:");
+	    for(int i=0; i<8; i++) 
+	       System.out.println(mystack2.pop());
+	}
+}
