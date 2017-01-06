12767b8b38ee732aa6307d6f4890a0645410de2a
marsamoeba <305609342@qq.com>
2017-01-06 14:20:47 +0800

    p155 String类的一些方法 equals() length() charAt()

--- src/chapter07/StringDemo2.java
+++ src/chapter07/StringDemo2.java
@@ -18,5 +18,15 @@ public class StringDemo2 {
 		System.out.println("Length of strOb1: " + strOb1.length());
 
 		System.out.println("Char at index 3 in strOb1: " + strOb1.charAt(3));
+		
+		if (strOb1.equals(strOb2))
+			System.out.println("strOb1 == strOb2");
+		else
+			System.out.println("strOb1 != strOb2");
+		
+		if (strOb1.equals(strOb3))
+			System.out.println("strOb1 == strOb3");
+		else
+			System.out.println("strOb1 != strOb3");
 	}
 }
