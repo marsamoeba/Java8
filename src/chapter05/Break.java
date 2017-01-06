df1e78e0e8202a753cf9a441faacd23a3e8726b8
marsamoeba <305609342@qq.com>
2017-01-06 14:50:28 +0800

    p105 使用break语句作为goto语句的一种形式
    暂时注释掉。

--- src/chapter05/Break.java
+++ src/chapter05/Break.java
@@ -8,20 +8,20 @@ package chapter05;
 // Using break as a civilized form of goto.
 public class Break {
 	public static void main(String[] args) {
-		boolean t = true;
+//		boolean t = true;
 		
 		// 三个嵌套代码块
-		first: {
-			second: {
-				third: {
-					System.out.println("Before the break.");
-					if (t)
-						break second; // 跳转到second标识的代码块末端
-					System.out.println("This won't execute");
-				}
-				System.out.println("This won't execute");
-			}
-			System.out.println("This is after second block");
-		}
+//		first: {
+//			second: {
+//				third: {
+//					System.out.println("Before the break.");
+//					if (t)
+//						break second; // 跳转到second标识的代码块末端
+//					System.out.println("This won't execute");
+//				}
+//				System.out.println("This won't execute");
+//			}
+//			System.out.println("This is after second block");
+//		}
 	}
 }
