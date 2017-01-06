f8a7fcb7002a1ea58df2c9886807f0d4dff8b78a
marsamoeba <305609342@qq.com>
2017-01-06 15:14:59 +0800

    p159 使用一个常规参数和一个可边长参数

---
+++ src/chapter07/VarArgs2.java
@@ -0,0 +1,24 @@
+package chapter07;
+
+/**
+ * p159 使用一个常规参数和一个可边长参数
+ * @author marsamoeba
+ *
+ */
+// Use varargs with standard arguments.
+public class VarArgs2 {
+	static void vaTest(String msg, int ... v) {
+		System.out.print(msg + v.length +  " Contents: "); 
+		
+		for (int x : v) 
+			System.out.print(x + " ");
+		
+		System.out.println();
+	}
+	
+	public static void main(String[] args) {
+		vaTest("One vararg: ", 10); 
+	    vaTest("Three varargs: ", 1, 2, 3);  
+	    vaTest("No varargs: ");  
+	}
+}
