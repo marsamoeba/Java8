0b25f24dcf95467f58be5ad504d72097d2223fd6
marsamoeba <305609342@qq.com>
2017-01-06 08:46:28 +0800

    p155 String类介绍

---
+++ src/chapter07/StringDemo.java
@@ -0,0 +1,20 @@
+package chapter07;
+
+/**
+ * p155 String类介绍 
+ * @author marsamoeba
+ *
+ */
+//Demonstrating Strings.
+class StringDemo {
+	public static void main(String[] args) {
+		String strOb1 = "First String";
+		String strOb2 = "Second String";
+		String strOb3 = strOb1 + " and " + strOb2;
+		
+		System.out.println(strOb1);
+		System.out.println(strOb2);
+		System.out.println(strOb3);
+		
+	}
+}
