feeba6e66572866fc1f9088faac19e197785bff9
marsamoeba <305609342@qq.com>
2017-01-06 14:23:46 +0800

    p156 字符串数组

---
+++ src/chapter07/StringDemo3.java
@@ -0,0 +1,16 @@
+package chapter07;
+
+/**
+ * p156 字符串数组
+ * @author marsamoeba
+ *
+ */
+// Demonstrate String arrays.
+class StringDemo3 {
+	public static void main(String[] args) {
+		String str[] = { "one", "two", "three" };
+		
+		for (int i = 0; i < str.length; i++) 
+			System.out.println("str[" + i + "]: " + str[i]);
+	}
+}
