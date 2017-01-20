31329ae15ecb9d4c9c7849dde48d13d17efb3dfe
marsamoeba <305609342@qq.com>
2017-01-20 17:41:17 +0800

    p200

---
+++ src/chapter09/p1/Demo.java
@@ -0,0 +1,14 @@
+package chapter09.p1;
+
+/**
+ * p200
+ * @author marsamoeba
+ *
+ */
+class Demo {
+	public static void main(String[] args) {
+		Protection ob1 = new Protection();
+	    Derived ob2 = new Derived();
+	    SamePackage ob3 = new SamePackage();
+	}
+}
