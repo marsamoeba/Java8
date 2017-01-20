c463028343df1267eeedbffa4867feddfb0e2f7c
marsamoeba <305609342@qq.com>
2017-01-20 17:42:11 +0800

    p200

--- src/chapter09/p1/Demo.java
+++ src/chapter09/p1/Demo.java
@@ -5,7 +5,7 @@ package chapter09.p1;
  * @author marsamoeba
  *
  */
-class Demo {
+public class Demo {
 	public static void main(String[] args) {
 		Protection ob1 = new Protection();
 	    Derived ob2 = new Derived();
