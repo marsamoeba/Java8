5180c7d21916f56751a4c1c63a2e059cd60b1d96
marsamoeba <305609342@qq.com>
2017-01-26 21:33:59 +0800

    p246 实现Runnable接口 创建新线程

---
+++ src/chapter11/ThreadDemo.java
@@ -0,0 +1,44 @@
+package chapter11;
+
+/**
+ * p246 实现Runnable接口 创建新线程
+ * @author marsamoeba
+ *
+ */
+class NewThread implements Runnable {
+	Thread t;
+	
+	NewThread() {
+		t =  new Thread(this, "Demo Thread");
+		System.out.println("Child thread: " + t);
+		t.start();
+	}
+	
+	public void run() {
+		try {
+			for (int i = 5; i > 0; i--) {
+				System.out.println("Child thread: " + i);
+				Thread.sleep(500);
+			}
+		} catch (InterruptedException e) {
+			System.out.println("Child interrupted.");
+			System.out.println("Exiting child thread.");
+		}
+	}
+}
+
+class ThreadDemo {
+	public static void main(String[] args) {
+		new  NewThread();
+		
+		try {
+			for (int i = 5; i > 0; i--) {
+				System.out.println("Main Thread: " + i);
+				Thread.sleep(1000);
+			}
+		} catch (InterruptedException e) {
+			System.out.println("Main thread interrupted.");
+		}
+		 System.out.println("Main thread exiting.");
+	}
+}
