5c8c25fe115e64affbabab9c0bd47d6c8b8bd8e7
marsamoeba <305609342@qq.com>
2017-02-03 16:55:36 +0800

    p253 使用同步方法 synchronized

--- src/chapter11/Synch.java
+++ src/chapter11/Synch.java
@@ -1,12 +1,13 @@
 package chapter11;
 
 /**
- * p253 使用同步方法
+ * p253 使用同步方法 synchronized
  * @author marsamoeba
  *
  */
 class Callme {
-	void call(String msg) {
+	// 添加同步方法 保证资源同步  
+	synchronized void call(String msg) {
 		System.out.print("[" + msg);
 		try {
 			Thread.sleep(1000);
@@ -18,6 +19,41 @@ class Callme {
 	}
 }
 
+class Caller implements Runnable {
+	String msg;
+	Callme target;
+	Thread t;
+	
+	public Caller(Callme targ, String s) {
+		target = targ;
+		msg = s;
+		t = new Thread(this);
+		t.start();
+	}
+	
+	@Override
+	public void run() {
+		target.call(msg);
+	}
+	
+}
+
+
 class Synch {
+	public static void main(String[] args) {
+		Callme target = new Callme();
+		Caller ob1 = new Caller(target, "Hello");
+	    Caller ob2 = new Caller(target, "Synchronized");
+	    Caller ob3 = new Caller(target, "World");
+	    
+	    try {
+			ob1.t.join();
+			ob2.t.join();
+			ob3.t.join();
+		} catch (InterruptedException e) {
+			System.out.println("Interrupted");
+			e.printStackTrace();
+		}
+	}
 
 }
